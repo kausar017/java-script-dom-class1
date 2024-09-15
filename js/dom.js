437e2b846e6653460ca2b647e604053783773422
kousra <mdkousarmia71@gmail.com>
2024-09-15 09:36:38 +0600

    java script

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,23 @@
-console.log('hello Frome The js');
+// console.log('hello Frome The js');
+//  // const fruits = document.getElementById('fruits-title');
+//         // fruits.innerText = 'fruits changed by js';
+//         // console.log(fruits);
+        
+//     //    const place = document.getElementsByClassName('places') ;
+//     //    for(const plac of place) { 
+//     //     console.log(plac.innerText); 
+        
+//     //    }
+//     const doc = document.querySelectorAll('.fruits-container li');
+//     //    console.log(doc);
+//        for(const li of doc) {
+//         console.log(li.innerText);
+        
+//        }
+// const ti = document.getElementById('fruits-title').style.textAlign= 'center';
+// console.log(ti);
+
+// const tor = document.getElementById('main-title').style.backgroundColor= 'yellow';
+// console.log(tor);
+
+// const my = document.getElementById('my-title').style.textColor= 'pink';
---
+++ js/stylejs.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+for( const section of sections) {
+    section.style.border = '2px solid steelblue';
+    section.style.marginBlock = '5px';
+    section.style.borderRadius = '10px';
+    section.style.padding = '20px';
+    section.style.backgroundColor = 'lightGray'
+    
+}
+// const sections = document.querySelectorAll('section li');
